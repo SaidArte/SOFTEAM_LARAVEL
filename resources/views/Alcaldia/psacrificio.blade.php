1b45352ca5ddb458ac5b8f0f9d9baa8b4737a999
Villalobos19 <dmcruzv@unah.hn>
2023-08-11 21:16:12 -0600

    Commit Botones
    Modificacion de los botones nuevo y editar

--- resources/views/Alcaldia/psacrificio.blade.php
+++ resources/views/Alcaldia/psacrificio.blade.php
@@ -12,6 +12,135 @@
         }
     </style>
 
+    <style>
+        /* Boton Nuevo */
+        .Btn {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            width: 40px;
+            height: 40px;
+            border-radius: calc(45px/2);
+            border: none;
+            cursor: pointer;
+            position: relative;
+            overflow: hidden;
+            transition-duration: .3s;
+            box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.199);
+            background-color: rgb(0, 143, 0);
+            }
+
+        /* plus sign */
+        .sign {
+            width: 100%;
+            font-size: 2.0em;
+            color: white;
+            transition-duration: .3s;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        /* text */
+        .text {
+            position: absolute;
+            right: 0%;
+            width: 0%;
+            opacity: 0;
+            color: white;
+            font-size: 1.0em;
+            font-weight: 300;
+            transition-duration: .3s;
+        }
+        /* hover effect on button width */
+        .Btn:hover {
+            width: 125px;
+            transition-duration: .3s;
+        }
+
+        .Btn:hover .sign {
+            width: 30%;
+            transition-duration: .3s;
+            padding-left: 15px;
+        }
+        /* hover effect button's text */
+        .Btn:hover .text {
+            opacity: 1;
+            width: 70%;
+            transition-duration: .3s;
+            padding-right: 15px;
+        }
+        /* button click effect*/
+        .Btn:active {
+            transform: translate(2px ,2px);
+        }
+    </style>
+    <style>
+        /* Estilos al boton editar */
+        .edit-button {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            background-color: rgb(0, 150, 255);
+            border: none;
+            font-weight: 600;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.164);
+            cursor: pointer;
+            transition-duration: 0.3s;
+            overflow: hidden;
+            position: relative;
+            text-decoration: none !important;
+            }
+
+        .edit-svgIcon {
+            width: 17px;
+            transition-duration: 0.3s;
+        }
+
+        .edit-svgIcon path {
+            fill: white;
+        }
+
+        .edit-button:hover {
+            width: 120px;
+            border-radius: 50px;
+            transition-duration: 0.3s;
+            background-color: rgb(0, 100, 255);
+            align-items: center;
+        }
+
+        .edit-button:hover .edit-svgIcon {
+            width: 20px;
+            transition-duration: 0.3s;
+            transform: translateY(60%);
+            -webkit-transform: rotate(360deg);
+            -moz-transform: rotate(360deg);
+            -o-transform: rotate(360deg);
+            -ms-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+
+        .edit-button::before {
+            display: none;
+            content: "Editar";
+            color: white;
+            transition-duration: 0.3s;
+            font-size: 2px;
+        }
+
+        .edit-button:hover::before {
+            display: block;
+            padding-right: 10px;
+            font-size: 13px;
+            opacity: 1;
+            transform: translateY(0px);
+            transition-duration: 0.3s;
+        }
+
+    </style>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
@@ -30,7 +159,11 @@
 
 @section('content')
     <p align="right">
-        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#psacrificio">+ Nuevo</button>
+        <button type="button" class="Btn" data-toggle="modal" data-target="#psacrificio">
+            <div class="sign">+</div>
+  
+            <div class="text">Nuevo</div>
+        </button>
     </p>
     <div class="modal fade bd-example-modal-sm" id="psacrificio" tabindex="-1">
         <div class="modal-dialog">
@@ -242,12 +375,18 @@
                     <td>{{$psacrificio['DIR_PSACRIFICIO']}}</td>
                     <td>{{$psacrificio['COD_ANIMAL']}}</td>
                     <td>
-                        <button value="Editar" title="Editar" class="btn btn-outline-info" type="button" data-toggle="modal" data-target="#psacrificio-edit-{{$psacrificio['COD_PSACRIFICIO']}}">
-                            <i class='fas fa-edit' style='font-size:13px;color:Orange'></i> Editar
+                        <button value="Editar" title="Editar" class="edit-button" type="button" data-toggle="modal" data-target="#psacrificio-edit-{{$psacrificio['COD_PSACRIFICIO']}}">
+                            <svg class="edit-svgIcon" viewBox="0 0 512 512">
+                                <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 
+                                480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 
+                                22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 
+                                325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 
+                                0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z"></path>
+                            </svg>
                         </button>
-                        <button value="Eliminar" title="Eliminar" class="btn btn-outline-danger custom-delete-button" type="button" data-toggle="modal" data-target="#psacrificio-delete-confirm" data-id="{{$psacrificio['COD_PSACRIFICIO']}}">
+                        <!--<button value="Eliminar" title="Eliminar" class="btn btn-outline-danger custom-delete-button" type="button" data-toggle="modal" data-target="#psacrificio-delete-confirm" data-id="{{$psacrificio['COD_PSACRIFICIO']}}">
                             <i class='fas fa-trash-alt' style='font-size:13px;color:Red'></i> Eliminar
-                        </button>
+                        </button>-->
                     </td>
                 </tr>
                 <!-- Modal for editing goes here -->
