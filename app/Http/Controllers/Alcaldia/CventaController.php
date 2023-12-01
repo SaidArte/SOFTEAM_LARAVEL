f3edee2f79ffcb58c09d0ac380784de10fe14411
FaniAviles98 <136545121+FaniAviles98@users.noreply.github.com>
2023-11-30 23:59:57 -0600

    commit como antes
    commit como antes funcional

--- app/Http/Controllers/Alcaldia/CventaController.php
+++ app/Http/Controllers/Alcaldia/CventaController.php
@@ -20,18 +20,17 @@ class CventaController extends Controller
       $citaArreglo = json_decode($Cventa->body(), true);
       // Imprime los datos para verificar si están llegando correctamente
       // dd($citaArreglo);
-     
+      $Animal = Http::withHeaders($headers)->get(self::urlapi.'ANIMAL/GETALL');
+      $AnimalArreglo = json_decode($Animal->body(), true);
       $personas = Http::withHeaders($headers)->get(self::urlapi.'PERSONAS/GETALL');
       $personasArreglo = json_decode($personas->body(), true);
       // Imprime los datos para verificar si están llegando correctamente
      // dd($citaArreglo);
-     $fierro = Http::withHeaders($headers)->get(self::urlapi.'FIERROS/GETALL');
-      $fierroArreglo = json_decode($fierro->body(), true);
 
      
       return view('Alcaldia.cventa')
       ->with('citaArreglo', $citaArreglo)
-      ->with('fierroArreglo', $fierroArreglo)
+      ->with('AnimalArreglo', $AnimalArreglo)
      ->with('personasArreglo', $personasArreglo); 
   
       //return view('Alcaldia.Cventa', compact('citaArreglo'));
@@ -50,23 +49,12 @@ class CventaController extends Controller
        
        // "COD_CVENTA"=> $request->input("COD_CVENTA"),
         //"FEC_CVENTA"=> $request->input("FEC_CVENTA"),
-       
-        "COD_PERSONA"=> $request->input("COD_PERSONA") ,
+        "COD_VENDEDOR"=> $request->input("COD_VENDEDOR") ,
         "NOM_COMPRADOR"=> $request->input("NOM_COMPRADOR"),
         "DNI_COMPRADOR"=> $request->input("DNI_COMPRADOR"),
         "COD_ANIMAL" => $request->input("COD_ANIMAL"),
         "FOL_CVENTA" => $request->input("FOL_CVENTA"),
         "ANT_CVENTA"=> $request->input("ANT_CVENTA") ,
-        "CLAS_ANIMAL"=> $request->input("CLAS_ANIMAL") ,
-        "RAZ_ANIMAL" => $request->input("RAZ_ANIMAL"),
-        "COL_ANIMAL"=> $request->input("COL_ANIMAL"),
-        "COD_FIERRO" => $request->input("COD_FIERRO"),
-        "VEN_ANIMAL"=> $request->input("VEN_ANIMAL") ,
-        "HER_ANIMAL" => $request->input("HER_ANIMAL"),
-        "CANT_CVENTA"=> $request->input("CANT_CVENTA") ,
-       "FOL_CVENTA" => $request->input("FOL_CVENTA"),
-       "ANT_CVENTA" => $request->input("ANT_CVENTA") ,
-       "IND_CVENTA"=> $request->input("IND_CVENTA"),
         //"CLAS_ANIMAL"=> $request->input("CLAS_ANIMAL") ,
        // "RAZ_ANIMAL" => $request->input("RAZ_ANIMAL"),
         //"COL_ANIMAL"=> $request->input("COL_ANIMAL"),
@@ -91,22 +79,12 @@ class CventaController extends Controller
        //"TABLA_NOMBRE"=>$request->input("TABLA_NOMBRE"),
        
         "COD_CVENTA"=>$request->input("COD_CVENTA") ,
-        "COD_PERSONA"=> $request->input("COD_PERSONA") ,
+        "COD_VENDEDOR"=> $request->input("COD_VENDEDOR") ,
         "NOM_COMPRADOR"=> $request->input("NOM_COMPRADOR"),
         "DNI_COMPRADOR"=> $request->input("DNI_COMPRADOR"),
         "COD_ANIMAL" => $request->input("COD_ANIMAL"),
         "FOL_CVENTA" => $request->input("FOL_CVENTA"),
         "ANT_CVENTA"=> $request->input("ANT_CVENTA") ,
-        "CLAS_ANIMAL"=> $request->input("CLAS_ANIMAL") ,
-        "RAZ_ANIMAL" => $request->input("RAZ_ANIMAL"),
-        "COL_ANIMAL"=> $request->input("COL_ANIMAL"),
-        "COD_FIERRO" => $request->input("COD_FIERRO"),
-        "VEN_ANIMAL"=> $request->input("VEN_ANIMAL") ,
-        "HER_ANIMAL" => $request->input("HER_ANIMAL"),
-        "CANT_CVENTA"=> $request->input("CANT_CVENTA") ,
-       "FOL_CVENTA" => $request->input("FOL_CVENTA"),
-       "ANT_CVENTA" => $request->input("ANT_CVENTA") ,
-       "IND_CVENTA"=> $request->input("IND_CVENTA"),
        // "CLAS_ANIMAL"=> $request->input("CLAS_ANIMAL") ,
         //"RAZ_ANIMAL" => $request->input("RAZ_ANIMAL"),
         //"COL_ANIMAL"=> $request->input("COL_ANIMAL"),
@@ -120,5 +98,6 @@ class CventaController extends Controller
        return redirect('/Cventa');
    }
 
+   
 
 }
